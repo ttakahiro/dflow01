8aca137687a11b52ec4c840aed7eae19ac275abd
Takahiro Takahashi <takahiro.takahashi@accelsofteng.com>
2019-08-03 00:08:47 +0900

    Add fp

--- dftest01.c
+++ dftest01.c
@@ -6,21 +6,24 @@ void sub2(char *);
 
 char *file_name;
 int main(int argc, char *argv[]){
+FILE  fp;
 	
 	file_name = getenv("TEST_FILE_NAME");
 	
 	printf("getenv done\n");
 	
-	fopen(file_name, "r");
+	fp = fopen(file_name, "r");
 
 	
 	sub1();
 	
 	sub2(argv[2]);
 	
+	fclose(fp);
+	
 	fopen("File_OPen", "r");
 	
-	
+
 	return 0;
 }
 
