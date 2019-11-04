ce7fdaa1b1783207235f26197ea44892d8f773ed
Takahiro Takahashi <takahiro.takahashi@accelsofteng.com>
2019-11-04 22:15:03 +0900

    Update test

--- dftest01.c
+++ dftest01.c
@@ -5,33 +5,33 @@ void sub1(void);
 void sub2(char *);
 
 typedef struct {
-	int  Order;
-	int  Date;
+    int  Order;
+    int  Date;
 }OrderStr;
 char *file_name;
 OrderStr  OneOrder;
 int main(int argc, char *argv[]){
 FILE  *fp;
-	
-	file_name = getenv("TEST_FILE_NAME");
-	
-	printf("getenv done\n");
-	
-	fp = fopen(file_name, "r");
+    
+    file_name = getenv("TEST_FILE_NAME");
+    
+    printf("getenv done\n");
+    
+    fp = fopen(file_name, "r");
 
-	
-	sub1();
-	
-	sub2(file_name);
-	
-	if(OneOrder.Date == 0)
-	   OneOrder.Date = 804;
-	   
-	fclose(fp);
-	
-	fopen("File_OPen", "r");
-	
+    
+    sub1();
+    
+    sub2(file_name);
+    
+    if(OneOrder.Date == 0)
+       OneOrder.Date = 804;
+       
+    fclose(fp);
+    
+    fopen("File_Open", "r");
+    
 
-	return 0;
+    return 0;
 }
 
